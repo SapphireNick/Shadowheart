f404528b9d3e2e41a5fec67f80f74b5fd4182a7f
Nikita-Nick Funk <nikita.nick.funk@gmail.com>
2021-03-04 17:26:08 +0100

    Added changes and fixed indentation

--- modules/renderer/include/r_shader.hh
+++ modules/renderer/include/r_shader.hh
@@ -3,49 +3,45 @@
 
 #include "glad/glad.h"
 
-#include <string>
 #include <fstream>
-#include <sstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
-namespace Shadowheart
-{
-    namespace Renderer
-    {
-        class Shader
-        {
-        private:
-            unsigned int _id;
+namespace Shadowheart {
+namespace Renderer {
+class Shader {
+private:
+  unsigned int _id;
 
-            public:
+public:
+  /**
+   * @brief Takes shader-paths, compiles and links shaders into a shader-program
+   * @param: const std::string& vertexShaderPath
+   *       : const std::string& fragmentShaderPath
+   * @return: void
+   */
+  Shader(const std::string &vertexShaderPath,
+         const std::string &fragmentShaderPath);
 
-            /**
-             * @brief Takes shader-paths, compiles and links shaders into a shader-program
-             * @param: const std::string& vertexShaderPath
-             *       : const std::string& fragmentShaderPath
-             * @return: void
-             */
-            Shader(const std::string& vertexShaderPath,
-                   const std::string& fragmentShaderPath);
+  /**
+   * @brief Destroys objects and frees / deletes associated shader-program
+   * @param: void
+   * @return: void
+   */
+  ~Shader();
 
-            /**
-             * @brief Destroys objects and frees / deletes associated shader-program
-             * @param: void
-             * @return: void
-             */
-            ~Shader();
+  /**
+   * @brief Activate / Use asociated shader-program
+   * @param: void
+   * @return: void
+   */
+  void use();
 
-            /**
-             * @brief Activate / Use asociated shader-program
-             * @param: void
-             * @return: void
-             */
-            void use();
+  // TODO: #5 convenience functions to access / modify uniforms
 
-            // TODO: #5 convenience functions to access / modify uniforms
-            
-        }; /* Shader */
-    } /* Renderer */
-} /* Shadowheart */
+}; /* Shader */
+} // namespace Renderer
+} // namespace Shadowheart
 
 #endif // R_SHADER_HH
