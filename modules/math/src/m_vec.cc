fb7a45586e1648048a2e88140e1c8f230f1485ca
Dennis Jakob <74840154+d-jakob@users.noreply.github.com>
2021-03-29 12:01:14 +0200

    implemented requested changes

--- modules/math/src/m_vec.cc
+++ modules/math/src/m_vec.cc
@@ -3,9 +3,6 @@
 namespace Shadowheart {
 namespace Math {
 
-Shadowheart::Math::vec1::vec1(const float &x) : _v{x} {}
-float& Shadowheart::Math::vec1::operator[](const int &i) { return _v[i]; }
-
 Shadowheart::Math::vec2::vec2(const float &x, const float &y) : _v{x, y} {}
 float& Shadowheart::Math::vec2::operator[](const int &i) { return _v[i]; }
 
@@ -14,9 +11,6 @@ float& Shadowheart::Math::vec3::operator[](const int &i) { return _v[i]; }
 
 Shadowheart::Math::vec4::vec4(const float &x, const float &y, const float &z, const float &w) : _v{x, y, z, w} {}
 float& Shadowheart::Math::vec4::operator[](const int &i) { return _v[i]; }
-
-Shadowheart::Math::ivec1::ivec1(const int &x) : _v{x} {}
-int& Shadowheart::Math::ivec1::operator[](const int &i) { return _v[i]; }
 
 Shadowheart::Math::ivec2::ivec2(const int &x, const int &y) : _v{x, y} {}
 int& Shadowheart::Math::ivec2::operator[](const int &i) { return _v[i]; }
