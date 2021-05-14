9ba17e8669de3834483eff62e08fc328757181f6
Nikita-Nick Funk <nikita.nick.funk@gmail.com>
2021-05-14 10:41:52 +0200

    Typo

--- modules/math/src/m_vec.cc
+++ modules/math/src/m_vec.cc
@@ -14,7 +14,7 @@ vec2 vec2::operator*(const float& a, const vec2& b) { return b * a; }
 vec3::vec3(const float& x, const float& y, const float& z) : _v{x, y, z} {}
 float& vec3::operator[](const int& i) { return _v[i]; }
 vec3::operator float*() { return _v; }
-vec3 vec3::operator+(const vec3& a) { return vec3(_v[0] + a._v[0], _v[1] + a._v[1], _v[2] + a._[2]); }
+vec3 vec3::operator+(const vec3& a) { return vec3(_v[0] + a._v[0], _v[1] + a._v[1], _v[2] + a._v[2]); }
 float vec3::operator*(const vec3& a) { return _v[0] * a._v[0] + _v[1] * a._v[1] + _v[2] * a._v[2]; }
 vec3 vec3::operator*(const float& a) { return vec3(_v[0] * a, _v[1] * a, _v[2] * a); }
 vec3 vec3::operator*(const float& a, const vec3& b) { return b * a; }
@@ -22,9 +22,7 @@ vec3 vec3::operator*(const float& a, const vec3& b) { return b * a; }
 vec4::vec4(const float& x, const float& y, const float& z, const float& w) : _v{x, y, z, w} {}
 float& vec4::operator[](const int& i) { return _v[i]; }
 vec4::operator float*() { return _v; }
-vec4& vec4::operator+(const vec4& a) {
-  return vec4(_v[0] + a._v[0], _v[1] + a._v[1], _v[2] + a._v[2], _v[3] + a._v[3]);
-}
+vec4 vec4::operator+(const vec4& a) { return vec4(_v[0] + a._v[0], _v[1] + a._v[1], _v[2] + a._v[2], _v[3] + a._v[3]); }
 float vec4::operator*(const vec4& a) { return _v[0] * a._v[0] + _v[1] * a._v[1] + _v[2] * a._v[2] + _v[3] + a._v[3]; }
 vec4 vec4::operator*(const float& a) { return vec4(_v[0] * a, _v[1] * a, _v[2] * a, _v[3] * a); }
 vec4 vec4::operator*(const float& a, const vec4& b) { return b * a; }
